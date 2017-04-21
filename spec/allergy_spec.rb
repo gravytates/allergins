88a5a753fae1a0d82a62944dfa84f6068770fd72
Grady Shelton <gradyish@gmail.com>
2017-04-20 19:24:48 -0700

    pass test to evaluate score of 92 which only lists largest possible allergins up to the score, skipping in order scores that are too large

--- spec/allergy_spec.rb
+++ spec/allergy_spec.rb
@@ -14,4 +14,8 @@ describe('Fixnum#allergy') do
   it('will evaluate a score of 6 as an shellfish and peanuts allergy') do
     expect(6.allergy).to(eq('shellfish, peanuts'))
   end
+
+  it('will evaluate a score of 92 as pollen, tomatoes, strawberries, and shellfish allergy') do
+    expect(92.allergy).to(eq('pollen, tomatoes, strawberries, shellfish'))
+  end
 end
